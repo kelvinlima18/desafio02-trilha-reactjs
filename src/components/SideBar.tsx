b9c0ec59939fc6ece8f149f57225e3c8a2909d17
Kelvin Lima <kelvinlima1812@gmail.com>
2021-06-15 12:55:50 -0300

    finale application

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,33 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface SidebarProps {
+  genres: GenreResponseProps[];
+  genreId: number;
+  handleClickButton: (id: number) => void;
+}
+
+export function SideBar({ genres, genreId, handleClickButton }: SidebarProps) {
+  return(
+    <nav className="sidebar">
+        <span>Watch<p>Me</p></span>
+
+        <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={genreId === genre.id}
+            />
+          ))}
+        </div>
+      </nav>
+  );
 }
